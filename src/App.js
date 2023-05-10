19eb9e885ebecc02cc06097c784e1d68ddb01b53
Udai <udai.arneja11ua@gmail.com>
2023-05-10 22:38:01 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Saran.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          What the hell are we doing buddy.
         </a>
       </header>
     </div>
